d2f22cdc1845c56c242dc9443210ba389df484d4
ParivalavanIT <112223851+ParivalavanIT@users.noreply.github.com>
2023-05-22 01:11:20 +0530

    initial commit

---
+++ javascript/index.js
@@ -0,0 +1,21 @@
+//vanta js library
+VANTA.NET({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: true,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  points: 15.0,
+  maxDistance: 19.0,
+  spacing: 20.0,
+  color: 0x57e8e8,
+  backgroundColor: 0x2d2d2d
+});
+// animate on scroll library
+ AOS.init({
+   duration: 1200,
+   once: false,
+ });
